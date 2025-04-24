593abdfc3add762f56b6baa723fc0a6313e24f7f
Yashawasthi71 <yashawasthi88@gmail.com>
2025-04-25 00:58:14 +0530

    update

--- stl11.cpp
+++ stl11.cpp
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-void printmap(map<int, string> m)
+void printmap(map<int, string> &m)
 {
     cout << "Size :" << m.size() << endl;
     for (auto &pr : m)
@@ -17,14 +17,14 @@ void printmap(map<int, string> m)
 int main()
 {
     map<int, string> m;
-    m[1] = "acb";
+    m[1] = "acb"; // 0 log(n)
     m[3] = "xyz";
     m[2] = "lmn";
 
     m.insert(make_pair(4, "pqr"));
     // m.insert({4,"pqr"}) ;
 
-    map<int, string>::iterator it;
+    // map<int, string>::iterator it;
     /* for (it = m.begin(); it != m.end(); it++)
     {
         cout << (*it).first << " " << (*it).second << endl;
@@ -33,6 +33,19 @@ int main()
     /* for (auto &pr : m)
     {
         cout << pr.first << " " << pr.second << endl;
+    }*/
+
+    auto it = m.find(3); // o(log(n))
+    // m.erase(2) ;
+    m.erase(it);
+    // m.clear();
+    /* if (it == m.end())
+    {
+        cout << "no value" << endl;
+    }
+    else
+    {
+        cout << (*it).first << " " << (*it).second << endl;
     }*/
 
     printmap(m);
